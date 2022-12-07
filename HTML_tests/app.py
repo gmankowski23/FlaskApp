88257138d746ad5425db60a7cfbfbf555047daa6
gmankowski23 <gmankowski23@bsmschool.org>
2022-12-07 08:31:08 -0600

    new Repo

---
+++ HTML_tests/app.py
@@ -0,0 +1,58 @@
+from re import U
+from flask import Flask, render_template, Response, request
+from time import sleep
+from static import stepper
+"""
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+AN2 = 23
+AN1 = 22
+DIG2 = 18
+DIG1 = 17
+GPIO.setup(AN2, GPIO.OUT)
+GPIO.setup(AN1, GPIO.OUT)
+GPIO.setup(DIG2, GPIO.OUT)
+GPIO.setup(DIG1, GPIO.OUT)
+p1 = GPIO.PWM(AN1, 100)
+p2 = GPIO.PWM(AN2, 100
+"""
+
+global current_speed
+current_speed = 0
+
+app = Flask(__name__) 
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def index(): 
+   if request.method == 'POST':    
+      return render_template('gamepad.html')
+   else:
+      return render_template('gamepad.html')
+
+@app.route('/forward', methods=['GET', 'POST'])
+def forward():
+   if request.method == 'POST':
+
+      joystick1 = request.form['joystick1']
+      joystick2 = request.form['joystick2']
+      joystick1 = int(float(joystick1)*100)
+      joystick2 = int(float(joystick2)*100)
+      print("Left % power",joystick1)
+      print("Right % power",joystick2)
+      if joystick1 < 0:
+         joystick1 = -joystick1
+         p1.start(joystick1)
+      else:
+         p1.start(joystick1)
+      if joystick2 < 0:
+         joystick2 = -joystick2
+         p2.start(joystick2)
+      else:
+         p2.start(joystick2)
+
+   return render_template('gamepad.html')
+
+if __name__ == '__main__': 
+	app.run(host='0.0.0.0', debug=True, threaded=True)
---
+++ HTML_tests/static/stepper.py
@@ -0,0 +1,14 @@
+
+
+def stepper (speed,current_speed):
+   speedlist = []
+   if -100 > speed > 100:
+      return
+   if current_speed < speed:
+      for i in range(current_speed, speed+1, (speed-current_speed)//5):
+         speedlist.append(i)
+   elif speed < current_speed:
+      for i in reversed(range(speed, current_speed+1, (current_speed-speed)//5)):    
+         speedlist.append(i)
+   current_speed = speed
+   return speedlist
